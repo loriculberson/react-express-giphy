39ce820344ae7e9ac5f896355f5bb93dc6d9b00f
Lori Culberson <lori_culberson@homedepot.com>
2019-05-03 09:40:21 -0400

    Add css modules and style app

--- client/src/RandomMedia.js
+++ client/src/RandomMedia.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import axios from "axios";
-import './RandomMedia.css';
+import styles from "./RandomMedia.module.css";
 
 const RandomMedia = () => {
-  const [ allMedia, setMedia ] = useState([{word: 'dog', imageUrl: `https://media3.giphy.com/media/bbshzgyFQDqPHXBo4c/200.gif`}])
+  const [ allMedia, setMedia ] = useState([])
 
   const mediaList = allMedia.map((media, index) => {
     return (
-      <div key={index}>
+      <div className={styles.card}key={index}>
         <img src={media.imageUrl} alt={media.dog}/>
         <p>{media.word}</p>
       </div>
@@ -17,13 +17,6 @@ const RandomMedia = () => {
   const updateMedia = (media) => setMedia([...allMedia, media])
 
   const generateMedia = async () => {
-    // console.log('hi from generateMedia!')
-    // fetch word from the wordnik api 
-    //pass word to giphy api
-    //send the object of word and image url to client
-    //console log the data in the console
-    //add the media to state []
-    //append the newest media to the DOM
     const newMedia = await axios.get('/get-new-word');
     console.log('media', newMedia.data)
     return updateMedia(newMedia.data)
@@ -32,11 +25,15 @@ const RandomMedia = () => {
   const clearMedia = () => setMedia([])
 
   return (
-    <div className="App">
-      <h1>Random Word & Giphy</h1>
-      <button onClick={generateMedia}>Surprise!</button>
-      <button onClick={clearMedia}>Clear</button>
-      {mediaList}
+    <div className={styles.app}>
+      <section className={styles.header}>
+        <h1>Random Word & Giphy</h1>
+        <button onClick={generateMedia}>Surprise!</button>
+        <button onClick={clearMedia}>Clear</button>
+      </section>
+      <section className={styles.cardList}>
+        {mediaList}
+      </section>
     </div>
   );
 };
